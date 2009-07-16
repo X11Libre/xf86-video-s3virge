c688ce43f935af22c23d58c692ffa501b88d9070
Peter Hutterer <peter.hutterer@who-t.net>
2009-07-16 11:54:36 +1000

    Update to xextproto 7.1 support.
    DPMS header was split into dpms.h (client) and dpmsconst.h (server). Drivers
    need to include dpmsconst.h if xextproto 7.1 is available.
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- src/s3v_driver.c
+++ src/s3v_driver.c
@@ -58,8 +58,13 @@ in this Software without prior written authorization from the XFree86 Project.
 		
 
 #include "globals.h"
+#ifdef HAVE_XEXTPROTO_71
+#include <X11/extensions/dpmsconst.h>
+#else
 #define DPMS_SERVER
 #include <X11/extensions/dpms.h>
+#endif
+
 
 #ifndef USE_INT10
 #define USE_INT10 0
